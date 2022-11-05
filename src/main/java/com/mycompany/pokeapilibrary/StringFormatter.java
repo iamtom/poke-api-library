b0904642481a65303f7c958b1315325ae621721c
Tom D <tomd.470@gmail.com>
2022-11-05 17:03:23 +0000

    StringFormatter now formats version names properly

--- src/main/java/com/mycompany/pokeapilibrary/StringFormatter.java
+++ src/main/java/com/mycompany/pokeapilibrary/StringFormatter.java
@@ -21,19 +21,63 @@ public class StringFormatter {
         return result;
     }
     
-    public static String formatVersionName(String input) {
-        //TODO account for black-2-white-2, omega-ruby-alpha-sapphire, ultra-sun-ultra-moon
-        //lets-go-pikachu-lets-go-eevee, 
+    public static String formatVersionName(String versionName) {
         
-        String versionName = basicFormat(input);
-        
-        if (versionName.contains(" ")) {
-            versionName = versionName.replace(" ", " & ");
+        //formatting for versions that aren't just one word
+        switch(versionName) {
+            case "red-blue":
+                versionName = "Red and Blue";
+                break;
+            case "gold-silver":
+                versionName = "Gold & Silver";
+                break;
+            case "ruby-sapphire":
+                versionName = "Ruby and Sapphire";
+                break;
+            case "firered-leafgreen":
+                versionName = "Fire Red & Leaf Green";
+                break;
+            case "diamond-pearl":
+                versionName = "Diamond & Pearl";
+                break;
+            case "heartgold-soulsilver":
+                versionName = "Heart Gold & Soul Silver";
+                break;
+            case "black-white":
+                versionName = "Black & White";
+                break;
+            case "xd":
+                versionName = "XD";
+                break;
+            case "black-2-white-2":
+                versionName = "Black 2 & White 2";
+                break;
+            case "x-y":
+                versionName = "X & Y";
+                break;
+            case "omega-ruby-alpha-sapphire":
+                versionName = "Omega Ruby & Alpha Sapphire";
+                break;
+            case "sun-moon":
+                versionName = "Sun & Moon";
+                break;
+            case "ultra-sun-ultra-moon":
+                versionName = "Ultra Sun & Ultra Moon";
+                break;
+            case "lets-go-pikachu-lets-go-eevee":
+                versionName = "Let's Go Pikachu & Let's Go Eevee";
+                break;
+            case "sword-shield":
+                versionName = "Sword & Shield";
+                break;
+            default:
+                break;
         }
         
-        if (versionName.equals("xd")) {
-            versionName = "XD";
-        }
+        //formatting for versions that are one word aside from XD version
+        if(!versionName.equals("XD")) {
+           versionName = basicFormat(versionName); 
+        }      
               
         return versionName;
     }
