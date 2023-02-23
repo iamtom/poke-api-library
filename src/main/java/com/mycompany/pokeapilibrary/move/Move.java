d64fab2c76db90f415cadfee1c44517b33e75b13
Tom D <tomd.470@gmail.com>
2023-02-23 23:18:07 +0000

    Added name property to Move

--- src/main/java/com/mycompany/pokeapilibrary/move/Move.java
+++ src/main/java/com/mycompany/pokeapilibrary/move/Move.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 //https://pokeapi.co/api/v2/move/{id} or {move name}
 
 public class Move {
+    private String name;
     @SerializedName("learned_by_pokemon")
     private ArrayList<NamedAPIResource> learnedByPokemon;
     //TODO: add more properties as required
@@ -17,7 +18,15 @@ public class Move {
 
     public void setLearnedByPokemon(ArrayList<NamedAPIResource> learnedByPokemon) {
         this.learnedByPokemon = learnedByPokemon;
-    }   
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String toString() {
